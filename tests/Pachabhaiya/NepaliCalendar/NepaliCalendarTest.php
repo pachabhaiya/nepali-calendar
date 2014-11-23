36192f36be37a35e66de509cb27b7a7283cd261e
Chhabi Pachabhaiya <c.pachabhaiya@gmail.com>
2014-11-22 21:38:53 -0600

    Added files for continuous integration testing.

---
+++ tests/Pachabhaiya/NepaliCalendar/NepaliCalendarTest.php
@@ -0,0 +1,78 @@
+<?php
+
+use Pachabhaiya\NepaliCalendar\NepaliCalendar;
+
+class NepaliCalendarTest extends PHPUnit_Framework_TestCase {
+
+	private $nepali_calendar;
+
+	public function setUp() {
+		$this->nepali_calendar = new NepaliCalendar;
+	}
+
+	public function tearDown() {
+		unset($this->nepali_calendar);
+	}
+
+	public function testInstanceOf() {
+		$this->assertInstanceOf('Pachabhaiya\NepaliCalendar\NepaliCalendar', $this->nepali_calendar);
+	}
+
+	public function testDate() {
+		$today = $this->nepali_calendar->getCurrentDate();
+		// print_r($this->nepali_calendar->getCurrentDate());
+		$this->assertEquals($today['Y'], '2071');
+	}
+
+	public function testEmpty() {
+		$stack = array();
+		$this->assertEmpty($stack);
+
+		array_push($stack, 'one');
+
+		return $stack;
+	}
+
+	/**
+   * @depends testEmpty
+   */
+  public function testPush(array $stack)
+  {
+
+  		// print_r(func_get_args());
+      array_push($stack, 'foo');
+      $this->assertEquals('foo', $stack[count($stack)-1]);
+      $this->assertNotEmpty($stack);
+
+      return $stack;
+  }
+
+
+  public function inputNumbers()
+  {
+    return array(
+      array(0, 0, 0),
+      array(0, 1, 1)
+    );
+  }
+
+  /**
+   * @dataProvider inputNumbers
+   */
+  public function testCanAddNumbers($x, $y, $sum)
+  {
+    $this->assertEquals($sum, $x + $y);
+  }
+
+
+
+
+  /**
+   * @depends testPush
+   */
+  // public function testPop(array $stack)
+  // {
+  //     $this->assertEquals('foo', array_pop($stack));
+  //     $this->assertEmpty($stack);
+  // }
+}
